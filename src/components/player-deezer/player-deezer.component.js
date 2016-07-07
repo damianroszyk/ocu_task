eacbd8e6262047aa66d29cb669106b49bcdc0d09
jakub.pikon <jakub.pikon@antrego.com>
2016-07-07 11:10:52 +0200

    Adding embedded Deezer player component

---
+++ src/components/player-deezer/player-deezer.component.js
@@ -0,0 +1,16 @@
+import angular from 'angular';
+import app from '../../app';
+
+import template from 'text!./player-deezer.html';
+import controller from './player-deezer.controller';
+
+let bindings = {
+    playlistId: '<',
+    width: '@?',
+    height: '@?'
+};
+
+export default angular
+    .module(app)
+    .component('playerDeezer', { template, controller, bindings })
+    .name;
---
+++ src/shared/constants/player.constant.js
@@ -0,0 +1,15 @@
+import angular from 'angular';
+
+import app from '../../app';
+
+const PLAYER_CONSTANT = {
+    defaultWidth: 300,
+    defaultHeight: 700,
+    deezerAppId: 1,
+    embeddedDeezerPlayerUrl: 'https://www.deezer.com/plugins/player'
+};
+
+export default angular
+    .module(app)
+    .constant('playerConstant', PLAYER_CONSTANT)
+    .name;
