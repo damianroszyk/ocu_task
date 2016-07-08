b8bfe1c6bc9b61ce633510b374c3662af93d14b7
jakub.pikon <jakub.pikon@antrego.com>
2016-07-08 09:42:48 +0200

    Adding category tiles layout

--- src/components/_home/home.controller.js
+++ src/components/_home/home.controller.js
@@ -1,9 +1,18 @@
 export default class HomeController {
     /*@ngInject*/
-    constructor() {
+    constructor($timeout) {
         this.categories = [
-            { name: 'category 1' },
-            { name: 'category 2' }
+            { name: 'category 1', featured: true },
+            { name: 'category 2' },
+            { name: 'category 3' },
+            { name: 'category 4' },
+            { name: 'category 5' },
+            { name: 'category 6' },
+            { name: 'category 7' },
+            { name: 'category 8' },
+            { name: 'category 9' },
+            { name: 'category 10' },
+            { name: 'category 11' }
         ];
 
         this.featuredPlaylists = [
