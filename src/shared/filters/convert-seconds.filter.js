2f79cc627391446415629d85b3bc7b2c8d4c6749
pawel.janicki <paweljanicki256@gmail.com>
2016-07-11 16:47:27 +0200

    adding filter that convert seconds to minutes or hours

--- src/shared/filters/convert-seconds.filter.js
+++ src/shared/filters/convert-seconds.filter.js
@@ -1,15 +1,31 @@
 import angular from 'angular';
 import app from '../../app';
 
-let convertSeconds = () => (input) => {
-    let minutes = Math.floor (input / 60);
-    let seconds = input % 60;
+let convertSeconds = () => (input, type) => {
 
-    if (seconds < 10) {
-        seconds = `0${seconds}`;
+    if (angular.isNumber(input)) {
+        let minutes = Math.floor(input / 60);
+        let seconds = input % 60;
+        let hours = 0;
+
+        if (minutes > 59) {
+            hours = Math.floor(minutes / 60);
+            minutes = minutes - (hours * 60);
+        }
+
+        if (seconds < 10) {
+            seconds = `0${seconds}`;
+        }
+
+        if (type === 'min') {
+            return `${minutes}:${seconds}`;
+        } else if (type === 'hour') {
+            return `${hours} hrs ${minutes} mins`;
+        } else {
+            return 0;
+        }
     }
 
-    return `${minutes}:${seconds}`;
 }
 
   export default angular
