73a06aabfe63cd3271d69fef2640558d0024e579
pawel.janicki <paweljanicki256@gmail.com>
2016-07-12 10:22:25 +0200

    improvements to the filter

--- src/shared/filters/convert-seconds.filter.js
+++ src/shared/filters/convert-seconds.filter.js
@@ -5,27 +5,21 @@ let convertSeconds = () => (input, type) => {
 
     if (angular.isNumber(input)) {
         let minutes = Math.floor(input / 60);
-        let seconds = input % 60;
+        let seconds = 0;
         let hours = 0;
 
-        if (minutes > 59) {
+        if (type === 'hours') {
             hours = Math.floor(minutes / 60);
             minutes = minutes - (hours * 60);
-        }
-
-        if (seconds < 10) {
-            seconds = `0${seconds}`;
-        }
-
-        if (type === 'min') {
-            return `${minutes}:${seconds}`;
-        } else if (type === 'hour') {
-            return `${hours} hrs ${minutes} mins`;
         } else {
-            return 0;
+            seconds = input % 60;
+            seconds = seconds < 10 ? `0${seconds}` : seconds;
         }
-    }
 
+        return type === 'hours' ?
+            `${hours} hrs ${minutes} mins` :
+            `${minutes}:${seconds}`;
+    }
 }
 
   export default angular
