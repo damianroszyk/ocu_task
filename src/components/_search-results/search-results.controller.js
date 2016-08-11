daea954ced8623b1901397f8297f1dc1908428a9
pawel.janicki <paweljanicki256@gmail.com>
2016-08-11 12:46:27 +0200

    adding playlist tile improvements

--- src/components/_search-results/search-results.controller.js
+++ src/components/_search-results/search-results.controller.js
@@ -1,9 +1,6 @@
-import _ from 'lodash';
-
 export default class SearchResultsController {
 	/* @ngInject */
-	constructor($scope, $location) {
-		var searchedText = _.split($location.path(), '/',3)[2];
-		$scope.searched = searchedText;
+	constructor() {
+
 	}
 }
