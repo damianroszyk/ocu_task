0cfe5600855c71c8ce9936ff1f6107e05851fea9
pawel.janicki <paweljanicki256@gmail.com>
2016-08-17 10:00:10 +0200

    interating playlist tile with playlists from server

--- src/components/_playlist/playlist.service.js
+++ src/components/_playlist/playlist.service.js
@@ -4,17 +4,21 @@ import app from 'app';
 
 class PlaylistService {
 	/* @ngInject */
-	constructor($http, $q, deezer, backendConstant, modelHelper) {
+	constructor($http, $q, deezer, backendConstant, domConstant, modelHelper) {
 		this.$http = $http;
 		this.$q = $q;
 		this.deezer = deezer;
 		this.modelHelper = modelHelper;
+		this.domConstant = domConstant;
 		this.backendConstant = backendConstant;
 		this.playlistBackend = this.modelHelper.buildUrl(
 			backendConstant.apiVersion, 'playlist'
 		);
 		this.searchBackend = this.modelHelper.buildUrl(
 			backendConstant.apiVersion, 'search', backendConstant.clientBrand
+		);
+		this.categoryBackend = this.modelHelper.buildUrl(
+			backendConstant.apiVersion, 'category/'
 		);
 	}
 
@@ -36,73 +40,20 @@ class PlaylistService {
 		);
 	}
 
-	getPlaylists(categoryId = null) {
-		return this.$q.when([{
-			id: 30595446,
-			name: 'playlist 1',
-			imageUrl: 'https://spotifyapps.blob.core.windows.net/sinfini-imagefiles/Playlist_BestClassicalNewReleasesSinfiniMusicPicks_Cover',
-			summary: 'Your hip hop hits in one playlist.',
-			description: 'The very best of new and recently released classical albums that we\'re listening to at Sinfinimusic for our reviews section. Updated every week with fresh repertoire and artists.',
-			nbTracks: 124,
-			duration: 10205
-		}, {
-			id: 30595446,
-			name: 'playlist 2',
-			imageUrl: 'https://spotifyapps.blob.core.windows.net/sinfini-imagefiles/Playlist_BestClassicalNewReleasesSinfiniMusicPicks_Cover',
-			summary: 'Your hip hop hits in one playlist.',
-			description: 'The very best of new and recently released classical albums that we\'re listening to at Sinfinimusic for our reviews section. Updated every week with fresh repertoire and artists.',
-			nbTracks: 124,
-			duration: 10205
-		}, {
-			id: 30595446,
-			name: 'playlist 3',
-			imageUrl: 'https://spotifyapps.blob.core.windows.net/sinfini-imagefiles/Playlist_BestClassicalNewReleasesSinfiniMusicPicks_Cover',
-			summary: 'Your hip hop hits in one playlist.',
-			description: 'The very best of new and recently released classical albums that we\'re listening to at Sinfinimusic for our reviews section. Updated every week with fresh repertoire and artists.',
-			nbTracks: 124,
-			duration: 10205
-		}, {
-			id: 30595446,
-			name: 'playlist 4',
-			imageUrl: 'https://spotifyapps.blob.core.windows.net/sinfini-imagefiles/Playlist_BestClassicalNewReleasesSinfiniMusicPicks_Cover',
-			summary: 'Your hip hop hits in one playlist.',
-			description: 'The very best of new and recently released classical albums that we\'re listening to at Sinfinimusic for our reviews section. Updated every week with fresh repertoire and artists.',
-			nbTracks: 124,
-			duration: 10205
-		}, {
-			id: 30595446,
-			name: 'playlist 5',
-			imageUrl: 'https://spotifyapps.blob.core.windows.net/sinfini-imagefiles/Playlist_BestClassicalNewReleasesSinfiniMusicPicks_Cover',
-			summary: 'Your hip hop hits in one playlist.',
-			description: 'The very best of new and recently released classical albums that we\'re listening to at Sinfinimusic for our reviews section. Updated every week with fresh repertoire and artists.',
-			nbTracks: 124,
-			duration: 10205
-		}, {
-			id: 30595446,
-			name: 'playlist 6',
-			imageUrl: 'https://spotifyapps.blob.core.windows.net/sinfini-imagefiles/Playlist_BestClassicalNewReleasesSinfiniMusicPicks_Cover',
-			summary: 'Your hip hop hits in one playlist.',
-			description: 'The very best of new and recently released classical albums that we\'re listening to at Sinfinimusic for our reviews section. Updated every week with fresh repertoire and artists.',
-			nbTracks: 124,
-			duration: 10205
-		}, {
-			id: 30595446,
-			name: 'playlist 7',
-			imageUrl: 'https://spotifyapps.blob.core.windows.net/sinfini-imagefiles/Playlist_BestClassicalNewReleasesSinfiniMusicPicks_Cover',
-			summary: 'Your hip hop hits in one playlist.',
-			description: 'The very best of new and recently released classical albums that we\'re listening to at Sinfinimusic for our reviews section. Updated every week with fresh repertoire and artists.',
-			nbTracks: 124,
-			duration: 10205
-		}, {
-			id: 30595446,
-			name: 'playlist 8',
-			imageUrl: 'https://spotifyapps.blob.core.windows.net/sinfini-imagefiles/Playlist_BestClassicalNewReleasesSinfiniMusicPicks_Cover',
-			summary: 'Your hip hop hits in one playlist.',
-			description: 'The very best of new and recently released classical albums that we\'re listening to at Sinfinimusic for our reviews section. Updated every week with fresh repertoire and artists.',
-			nbTracks: 124,
-			duration: 10205
-		}]);
+	addDefaultData(playlists) {
+		angular.forEach(playlists, playlist => {
+			playlist.description = 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.';
+			playlist.imageUrl = this.domConstant.defaultCategoryTileImage;
+		});
+		return playlists;
 	}
+
+	getPlaylists(categoryId) {
+		return this.$http.get(this.categoryBackend + categoryId + '/playlists').then(response =>
+			this.addDefaultData(response.data)
+		);;
+	}
+
 	getPlaylist(playlistId) {
 		return this.deezer.getPlaylist(playlistId);
 	}
