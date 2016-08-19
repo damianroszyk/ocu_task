c31ed4f540b06bd3bfe3c22581ea9f3c67eb15aa
pawel.janicki <paweljanicki256@gmail.com>
2016-08-19 17:31:56 +0200

    sort dropdown styling

--- src/components/_search-results/search-results.controller.js
+++ src/components/_search-results/search-results.controller.js
@@ -1,13 +1,22 @@
+import _ from 'lodash';
+
 export default class SearchResultsController {
 	/* @ngInject */
 	constructor($state, playlistService) {
 		this.$state = $state;
 		this.playlistService = playlistService;
 		this.order = this.$state.params.order;
+		this.sortOptions = [
+			{ value: 'updated', name : 'Recently updated'},
+			{ value: 'newest', name : 'Newest first'},
+			{ value: 'length', name : 'Length of time'}
+		];
+		this.chosenSortingOption = _.find(this.sortOptions, { value : this.$state.params.order});
 	}
-	sortSearchResults() {
-		let order = this.order;
+	setSortingParam(option) {
+		let order = option.value;
 		let sort = 'asc';
+		this.chosenSortingOption = option;
 		this.$state.go('searchResults', { order, sort }, { reload : true });
 	}
 }
