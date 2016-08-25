b5266ed7e4deff1f8f28d3ee4ce7afd37110ecf5
pawel.janicki <paweljanicki256@gmail.com>
2016-08-25 14:58:50 +0200

    navigation dropdown styling

--- src/components/categories-dropdown/categories-dropdown.controller.js
+++ src/components/categories-dropdown/categories-dropdown.controller.js
@@ -1,25 +1,12 @@
 export default class CategoriesDropdownController {
 	/* @ngInject */
-	constructor(categoryService) {
+	constructor($scope, categoryService, $stateParams) {
 		categoryService
 			.getCategories()
 			.then(response => this.categories = response);
+		$scope.$on('$stateChangeSuccess', this.closeDropdown.bind(this));
 	}
 	closeDropdown() {
 		this.isShown = false;
-	}
-	isActive(category, l1Category) {
-		return category.name === l1Category ? 'active' : '';
-	}
-	showCategoryChildren(category, target) {
-		this[target] = category.children;
-	}
-	hideCategoryChildren(target) {
-		this[target] = [];
-	}
-	hideSubcategories() {
-		this.l2Children = [];
-		this.l3Children = [];
-		this.l4Children = [];
 	}
 }
