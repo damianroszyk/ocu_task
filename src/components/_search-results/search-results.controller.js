c364ea1abb96dc4c7148be7639f61701bea013f9
pawel.janicki <paweljanicki256@gmail.com>
2016-08-26 10:09:28 +0200

    removing unnecessary line

--- src/components/_search-results/search-results.controller.js
+++ src/components/_search-results/search-results.controller.js
@@ -15,8 +15,7 @@ export default class SearchResultsController {
 	}
 	setSortingParam(option) {
 		let order = option.value;
-		let sort = 'asc';
-		sort = (order === 'newest') ? 'desc' : 'asc';
+		let sort = (order === 'newest') ? 'desc' : 'asc';
 		this.chosenSortingOption = option;
 		this.$state.go('searchResults', { order, sort }, { reload : true });
 	}
