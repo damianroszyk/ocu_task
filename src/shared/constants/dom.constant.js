a983687500f3a866b3ede6e59cce70dc8951024f
jakub.pikon <jakub.pikon@antrego.com>
2016-08-31 10:52:14 +0200

    Updating default playlist image

--- src/shared/constants/dom.constant.js
+++ src/shared/constants/dom.constant.js
@@ -11,7 +11,7 @@ const DOM_CONSTANT = {
 	},
 	defaultHeaderBackgroundImage: 'http://digster.fm/files/2015/09/1920x450-lores.jpg',
 	defaultBrandImage: 'http://digster.fm/wp-content/themes/digster/images/logo-header.png',
-	defaultCategoryTileImage: 'https://spotifyapps.blob.core.windows.net/sinfini-imagefiles/MainMenu_Instrument_MainImage_270x270'
+	defaultCategoryTileImage: 'shared/images/default_artist_cover.png'
 };
 
 export default angular
