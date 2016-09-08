626ee636ebad87a2db74ab6ae94631cdaa27620b
pawel.janicki <paweljanicki256@gmail.com>
2016-09-08 13:24:32 +0200

    start building follow functionality

--- src/components/playlist-tile/playlist-tile.controller.js
+++ src/components/playlist-tile/playlist-tile.controller.js
@@ -1,13 +1,18 @@
 export default class PlaylistTileController {
 	/* @ngInject */
-	constructor($state, $scope, Analytics, domConstant) {
+	constructor($state, $scope, Analytics, domConstant, storage) {
 		this.$state = $state;
 		this.$scope = $scope;
 		this.Analytics = Analytics;
 		this.defaultPlaylistTileImage = domConstant.defaultCategoryTileImage;
+		this.storage = storage;
 	}
 	onFollowClick() {
 		let category = this.$state.is('home') ? 'Home' : 'Category';
 		this.Analytics.trackEvent(category, 'Follow', this.playlist.name);
+
+		if (this.storage.getStorageProperty('preferredService').name === 'spotify') {
+			console.log('spotify');
+		}
 	}
 }
