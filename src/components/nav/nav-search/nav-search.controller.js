7fdd8fce6d805ff49bd78b6b8a12e4cf37f23477
Cezary Bedlewicz <cezary.bedlewicz@miquido.com>
2016-09-23 16:17:43 +0200

    search menu reworked

---
+++ src/components/nav/nav-search/nav-search.controller.js
@@ -0,0 +1,9 @@
+export default class NavSearchController {
+	/* @ngInject */
+	constructor($scope) {
+		let clearQuery = () => {
+			this.query = '';
+		};
+		$scope.$on('$stateChangeSuccess', clearQuery);
+	}
+}
