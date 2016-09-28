e20aea3ef0504547aa8a4602c2936bcafc3f0b31
pawel.janicki <paweljanicki256@gmail.com>
2016-09-28 18:07:08 +0200

    improving nav bar on mobile

--- src/components/nav/music-provider-dropdown/music-provider-dropdown.controller.js
+++ src/components/nav/music-provider-dropdown/music-provider-dropdown.controller.js
@@ -2,16 +2,8 @@ import _ from 'lodash';
 
 export default class MusicProviderDropdown {
 	/* @ngInject */
-	constructor($scope, musicProvider) {
+	constructor($scope) {
 		$scope.$on('$stateChangeSuccess', this.closeDropdown.bind(this));
-		this.musicProvider = musicProvider;
-		this._checkMusicProvider();
-		musicProvider.registerObserver(() => this._checkMusicProvider());
-	}
-	_checkMusicProvider() {
-		if (this.musicProvider.isSet()) {
-			this.poviderIsSet = true;
-		}
 	}
 	closeDropdown() {
 		this.isShown = false;
