5e5cab7e3a762fa273c339b83f26c2a9b74103f0
Cezary Bedlewicz <cezary.bedlewicz@miquido.com>
2016-09-29 11:45:58 +0200

    Minor fix

--- src/components/message-popup/message-popup.controller.js
+++ src/components/message-popup/message-popup.controller.js
@@ -2,15 +2,18 @@
 
 export default class MessagePopupController {
 	/* @ngInject */
-	constructor(messagePopupService) {
+	constructor($scope, messagePopupService) {
 		this.message = {};
+		this.$scope = $scope;
 		this.messagePopupService = messagePopupService;
 		messagePopupService.registerObserver(this.showPopup.bind(this));
 	}
 	showPopup(){
 		this.message = this.messagePopupService.message;
+		$scope.apply();
 	}
 	closePopup(){
 		this.message = {};
+		$scope.apply();
 	}
 }
